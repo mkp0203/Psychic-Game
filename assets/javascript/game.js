10d203e5a65e760e0f8b86170bb35e6b5407969f
Ghost <mkp0203@live.com>
2018-07-15 20:24:15 -0400

    actual final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,43 @@
+// Global Variables
 var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var soFar = [];
 
+// onkeydown event loop
 document.onkeydown = function (event) {
+    // local variables
     var userGuess = event.key;
     var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
 
+    // user wins
     if (userGuess === compGuess) {
         wins++;
         guessesLeft = 10;
         soFar = [];
     }
     
+    // users incorrect guess/losses
     if (userGuess !== compGuess) {
         guessesLeft--;
         soFar.push(userGuess.toUpperCase());
     }
     
+    // remaining guesses reaches 0
     if (guessesLeft === 0) {
         losses++;
         guessesLeft = 10;
         soFar = [];
     }
-    
-    for (var i = 0; i < soFar.length; i++) {
-        
-    }
 
-    var html =
+    // store actions in the "events" variable and present on the DOM 
+    var events =
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
         "<p>Remaining: " + guessesLeft + "</p>" +
         "<p>Guesses: " + soFar + "</p>";
 
-    document.querySelector("#game").innerHTML = html;
+    // links the "events" variable to the html "#game"
+    document.querySelector("#game").innerHTML = events;
 }
